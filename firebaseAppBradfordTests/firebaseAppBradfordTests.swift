d0a6ad2d1b16c087edacbd67eb8ff7ad99a7eeca
cameron bradford <kylocam0712@gmail.com>
2025-02-10 08:23:00 -0600

    Initial Commit

---
+++ firebaseAppBradfordTests/firebaseAppBradfordTests.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseAppBradfordTests.swift
+//  firebaseAppBradfordTests
+//
+//  Created by CAMERON BRADFORD on 2/10/25.
+//
+
+import Testing
+@testable import firebaseAppBradford
+
+struct firebaseAppBradfordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
